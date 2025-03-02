b708235d932e0e24369f5c84b9a7381efc993fc8
Henadzi Bessonay <s.client@mail.ru>
2025-03-02 19:54:53 +0300

    homework main update

--- src/main/java/ru/otus/java/basic/homeworks/homework7/transport/Transport.java
+++
@@ -1,5 +0,0 @@
-package ru.otus.java.basic.homeworks.homework7.transport;
-
-public interface Transport {
-    boolean moveToDistance(int distance, TypesOfTerrain typesOfTerrain);
-}
--- src/main/java/ru/otus/java/basic/homeworks/homework7/transport/TypesOfTerrain.java
+++
@@ -1,5 +0,0 @@
-package ru.otus.java.basic.homeworks.homework7.transport;
-
-public enum TypesOfTerrain {
-    DENSE_FOREST, PLAIN, SWAMP
-}
